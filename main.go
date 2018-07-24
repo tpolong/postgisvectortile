780fb070047e3e1851aefb4865088a177ac03717
Tangweilong <tpolong@users.noreply.github.com>
2018-07-24 17:54:06 +0800

    Update main.go
    performace ,remove no use request

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ import
 (   
     _ "github.com/lib/pq"
     "database/sql"
+	//"time"
+	"math"
     "log"
-    "math"
     "errors"
+	"fmt"
     "net/http"
     "regexp"
-    "strconv"
-    "strings"
+	"strconv"
+	"strings"
 )
 
 func tilePathToXYZ(path string) (TileID, error) {
@@ -56,11 +58,29 @@ func FloatToString(input_num float64) string {
     return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 func isIntersect(xmin float64,ymin float64,xmax float64,ymax float64, txmin float64,tymin float64,txmax float64,tymax float64) bool {
-	
+	// rectminx := math.Max(xmin, txmin)
+	// rectminy := math.Max(ymin, tymin)
+	// rectmaxx := math.Min(xmax, txmax)
+	// rectmaxy := math.Min(ymax, tymax)
+	// if(rectminx < rectmaxx && rectminy < rectmaxy){
 	if( xmin> txmax || xmax<txmin || ymin> tymax || ymax<tymin){
+		fmt.Println(1)
 		return false
 	}else{
+		// a:=(xmax-xmin)*(ymax-ymin)
+		// b:=(txmax-xmin)*(tymax-ymin)
+		// s:= (rectmaxx-rectminx)*(rectmaxy-rectminy) 
+		// fmt.Println(a,b,s)
+		// if( s<3/4*b && a<b){
+		// 	fmt.Println(2)
+		// 	return true
+		// }else{
+		// 	fmt.Println(3)
+		// 	return false
+		// }
+		fmt.Println(2)
 		return true
+		
 	}
 }
 func main(){
@@ -93,12 +113,17 @@ func main(){
 		if error != nil {
 			log.Fatal(error)
 		}
-		
+		rymax :=FloatToString(tymax)
+		rymin := FloatToString(tymin)
+		rxmin := FloatToString(txmin)
+		rxmax := FloatToString(txmax)
+		r := []string{rxmin,rymin,rxmax,rymax}
+		rmaxmin:=strings.Join(r, ",") 
 	mux.HandleFunc(tileBase, func(w http.ResponseWriter, r *http.Request) {
-		//t2 := time.Now() 
-		//log.Printf("url: %s", r.URL.Path)
+		//t := time.Now() 
+		
 		tilePart := r.URL.Path[len(tileBase):]
-		//fmt.Println("tilePart: ", tilePart)
+		
 		xyz, err := tilePathToXYZ(tilePart)
 		//fmt.Println("xyz: ", xyz)
 		
@@ -114,25 +139,38 @@ func main(){
 		symin := FloatToString(ymin);
 		sxmin := FloatToString(xmin);
 		sxmax := FloatToString(xmax);
-		// fmt.Println("ymax: ", ymax)
-		// fmt.Println("ymin: ",ymin)
-		// fmt.Println("xmin : ",xmin )
-		// fmt.Println("xmax : ",xmax )
+
 		isTrue :=isIntersect(xmin,ymin,xmax,ymax, txmin,tymin,txmax,tymax)
+		
+		s := []string{sxmin,symin,sxmax,symax}
+		maxmin:=strings.Join(s, ",") 
+		
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+
+		sql2 :="select case when 1/2.0*ST_Area(m.geom)>ST_Area(ST_Intersection(m.geom,n.geom)) and ST_Area(m.geom)>ST_Area(n.geom) then true else false end from (select ST_MakeEnvelope("+rmaxmin+", 4326) geom) m,(select ST_MakeEnvelope("+maxmin+",4326) geom ) n"
 		if(isTrue==false){
 			
 			http.Error(w, "Invalid tile url", 400)
 		}else{
+			var  i bool
+			fmt.Println(sql2)
+			rows2:= db.QueryRow(sql2)
+		err2 := rows2.Scan(&i)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		if i== false {			
+			http.Error(w, "Invalid tile url", 400)
+			return
+		}
 			//fmt.Println("Successfully connected!")
-		var tile []byte
-		s := []string{sxmin,symin,sxmax,symax}
-		maxmin:=strings.Join(s, ",") 
 		
+		var tile []byte
 		//  s2 := []string{" where (x between", xmin,"and",xmax,") and ( y between",ymin,"and",ymax,")"}
 		// wmaxmin:=strings.Join(s2, " ") 
-		sql:="SELECT ST_AsMVT(tile,'points',4096,'geom') tile  FROM (SELECT w.id,w.v,ST_AsMVTGeom(w.the_geom,Box2D(ST_MakeEnvelope("+maxmin+", 4326)),4096, 0, true)	 AS geom FROM "+table+" w) AS  tile where  tile.geom is not null"
+		sql:="SELECT ST_AsMVT(tile,'points',4096,'geom') tile  FROM (SELECT w.v,ST_AsMVTGeom(w.the_geom,Box2D(ST_MakeEnvelope("+maxmin+", 4326)),4096, 0, true)	 AS geom FROM "+table+" w) AS  tile where  tile.geom is not null"
+		//sql:="SELECT ST_AsMVT(tile,'points',4096,'geom') tile  FROM (SELECT w.id,w.v,ST_AsMVTGeom(w.the_geom,Box2D(p.geom),4096, 0, true)	 AS geom FROM "+table+" w,( select ST_MakeEnvelope("+maxmin+", 4326) geom) p  where w.the_geom && p.geom=TRUE ) AS  tile where  tile.geom is not null"
 		//fmt.Println(sql)
 		rows1:= db.QueryRow(sql)
 		err1 := rows1.Scan(&tile)
@@ -148,13 +186,21 @@ func main(){
 			http.Error(w, "Invalid tile url", 400)
 			return
 		}
+		//log.Printf("url: %s", r.URL.Path)
+		//fmt.Println("tilePart: ", tilePart)
+	                //fmt.Println("ymax: ", ymax)
+		//fmt.Println("ymin: ",ymin)
+		//fmt.Println("xmin : ",xmin )
+		//fmt.Println("xmax : ",xmax )
+		fmt.Println("extent: ",xmin,ymin,xmin,ymax,xmax,ymax,xmax,ymin,xmin,ymin )
 		w.Header().Set("Content-Type", "application/x-protobuf")
 		// w.Header().Set("Access-Control-Allow-Origin", "*")
 		// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Write(tile)
 		}
 		
-		//elapsed2 := time.Since(t2)
+		//elapsed := time.Since(t)
+		//fmt.Println(elapsed)
 		
 	})
 	log.Fatal(http.ListenAndServe(":8081", mux))
